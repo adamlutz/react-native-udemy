684a8646fe8432eead9808b801f0c3fc96998f62
Adam Luz <amlutz16@gmail.com>
2020-02-21 14:52:34 -0600

    props destructuring

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,17 +2,18 @@ import React from "react";
 import { Text, StyleSheet, View, Button } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 
-const HomeScreen = (props) => {
+const HomeScreen = ({ navigation }) => {
   // console.log(props.navigation)
 
+  // destructuring (es2015) -- pull out navigation object only from props object
   return (
     <View>
       <Button title='Components Demo'
-      onPress={() => { props.navigation.navigate('Components')} } />
+      onPress={() => { navigation.navigate('Components')} } />
       <Text style={styles.text}>Hi</Text>
 
       <TouchableOpacity
-        onPress={() =>  { props.navigation.navigate('List')} } >
+        onPress={() =>  { navigation.navigate('List')} } >
         <Text style={styles.text}>Go To List Demo</Text>
       </TouchableOpacity>
     </View>
