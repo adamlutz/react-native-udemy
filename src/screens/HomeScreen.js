46266875e32a2d2d985757963af01ee3150681ed
Adam Luz <amlutz16@gmail.com>
2020-02-21 14:36:24 -0600

    RN navigation fixes

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { Text, StyleSheet, View, Button } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const HomeScreen = () => {
   return (
     <View>
-      <Button title='Components Demo' />
+      <Button title='Components Demo'
+      onPress={() => {console.log('here')}} />
       <Text style={styles.text}>Hi</Text>
+
+      <TouchableOpacity
+        onPress={() => {console.log('list pressed')}} >
+        <Text style={styles.text}>Go To List Demo</Text>
+      </TouchableOpacity>
     </View>
   )};
 
