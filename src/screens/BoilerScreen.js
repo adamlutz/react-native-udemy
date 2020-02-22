25e71b5304bf06bdbde1f295cc6062dfc08ea6ca
Adam Luz <amlutz16@gmail.com>
2020-02-22 17:08:10 -0600

    adding boiler plate for all code examples

---
+++ src/screens/BoilerScreen.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+
+const CounterScreen = () => {
+
+  const friends = [];
+  // key are for performance optimizations
+  // esp for making updates to list
+  // horizontal / horizontal={true} property
+  return (
+    <View>
+        <Text>Current Count</Text>
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({});
+
+export default CounterScreen;
