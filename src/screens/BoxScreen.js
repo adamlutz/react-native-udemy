5f9b02c673920b02702ef5478697c0b4a1f763c1
Adam Luz <amlutz16@gmail.com>
2020-02-25 11:29:55 -0600

    flexbox example code

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -15,25 +15,33 @@ const styles = StyleSheet.create({
   viewStyle: {
     // alignItems: 'stretch',  // center, flex-start, flex-end, default is stretch
     flexDirection: 'row',  // or column (default)
-    // justifyContent: 'space-between', // flex-start, flex-end, center etc
+    justifyContent: 'space-between', // flex-start, flex-end, center etc
     borderWidth: 3,
     borderColor: 'black',
-    height: 200
+    height: 100,
+    // width: 120,
+    // alignItems: 'center'
   },
   textStyle: {
     borderWidth: 3,
-    flex: 4,            // take up as much space as possible inside parent
+    height: 50,
+    // flex: 4,            // take up as much space as possible inside parent
                         // (when listed alone)
     borderColor: 'red',
   },
   textTwoStyle: {
     borderWidth: 3,
-    flex: 2,            // relative % when used in concert w/ other flex values
+    height: 50,
+    // flex: 2,            // relative % when used in concert w/ other flex values
     borderColor: 'red',
+    alignSelf: 'flex-end',  // overrides alignItems on parent
+    //...StyleSheet.absoluteFillObject,
+    // position: 'absolute'
   },
   textThreeStyle: {
     borderWidth: 3,
-    flex: 2,
+    height: 50,
+    // flex: 2,
     borderColor: 'red',
   }
 });
